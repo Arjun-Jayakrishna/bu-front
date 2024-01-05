1c7f614d8136bc5b38e4559cc1ab5eed595a5053
Meg <meghanaskumar18@gmail.com>
2024-01-04 17:58:50 -0800

    Update pages

--- mockups/script/contribute.js
+++ mockups/script/contribute.js
@@ -6,18 +6,21 @@ const paragraph2 = document.getElementById('paragraph2');
 const next1 = document.getElementById('con-c-next1');
 const next2 = document.getElementById('con-c-next2');
 const next3 = document.getElementById('con-c-next3');
+const next5 = document.getElementById('con-c-next5');
 const previous2 = document.getElementById('con-c-previous2');
 const previous3 = document.getElementById('con-c-previous3');
 const c_page1 = document.getElementById('con-c-page1');
 const c_page2 = document.getElementById('con-c-page2');
 const c_page3 = document.getElementById('con-c-page3');
 const c_page4 = document.getElementById('con-c-page4');
+const c_page5 = document.getElementById('con-c-page5');
+
 
 
 
 const button3 = document.getElementById('button3');
 const paragraph3 = document.getElementById('paragraph3');
-
+const button5 = document.getElementById('button5');
 
 button1.addEventListener('click', () => {
     paragraph1.style.display = 'block';
@@ -91,4 +94,27 @@ button3.addEventListener('click', () => {
 
 });
 
+button5.addEventListener('click', () => {
+    paragraph1.style.display = 'none';
+    paragraph2.style.display = 'none';
+    paragraph3.style.display = 'block';
+    button6.style.backgroundColor = '#EF7F1A';
+    button5.style.backgroundColor = 'transparent';
+});
 
+
+next5.addEventListener('click', () => {
+c_page5.style.display = 'none'
+c_page6.style.display = 'block'
+
+
+
+
+});
+
+previous5.addEventListener('click', () => {
+c_page5.style.display = 'block'
+c_page6.style.display = 'none'
+
+
+});
