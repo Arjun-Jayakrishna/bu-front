422d912f0d63430332985b55dc8c44a0197da28a
Arjun-Jayakrishna <arjunjk740408@gmail.com>
2024-01-06 12:44:46 -0500

    feat: advance con-page mockup

--- mockups/script/contribute.js
+++ mockups/script/contribute.js
@@ -1,43 +1,43 @@
-const button1 = document.getElementById('button1');
-const paragraph1 = document.getElementById('paragraph1');
+const info_button = document.getElementById('info_button');
+const info_slide = document.getElementById('info_slide');
 
-const button2 = document.getElementById('button2');
-const paragraph2 = document.getElementById('paragraph2');
+const contribute_button = document.getElementById('contribute_button');
+const contribute_slide = document.getElementById('contribute_slide');
 const next1 = document.getElementById('con-c-next1');
 const next2 = document.getElementById('con-c-next2');
 const next3 = document.getElementById('con-c-next3');
 const next5 = document.getElementById('con-c-next5');
 const previous2 = document.getElementById('con-c-previous2');
 const previous3 = document.getElementById('con-c-previous3');
-const c_page1 = document.getElementById('con-c-page1');
-const c_page2 = document.getElementById('con-c-page2');
-const c_page3 = document.getElementById('con-c-page3');
-const c_page4 = document.getElementById('con-c-page4');
+const c_page1 = document.getElementById('contribute_slide1');
+const c_page2 = document.getElementById('contribute_slide2');
+const c_page3 = document.getElementById('contribute_slide3');
+const c_page4 = document.getElementById('contribute_slide4');
 const c_page5 = document.getElementById('con-c-page5');
 
 
 
 
-const button3 = document.getElementById('button3');
-const paragraph3 = document.getElementById('paragraph3');
+const merch_button = document.getElementById('merch_button');
+const merch_slides = document.getElementById('merch_slides');
 const button5 = document.getElementById('button5');
 
-button1.addEventListener('click', () => {
-    paragraph1.style.display = 'block';
-    paragraph2.style.display = 'none';
-    paragraph3.style.display = 'none';
-    button1.style.backgroundColor = '#EF7F1A';
-    button2.style.backgroundColor = 'transparent';
-    button3.style.backgroundColor = 'transparent';
+info_button.addEventListener('click', () => {
+    info_slides.style.display = 'block';
+    contribute_slides.style.display = 'none';
+    merch_slides.style.display = 'none';
+    info_button.style.backgroundColor = '#EF7F1A';
+    contribute_button.style.backgroundColor = 'transparent';
+    merch_button.style.backgroundColor = 'transparent';
 });
 
-button2.addEventListener('click', () => {
-    paragraph1.style.display = 'none';
-    paragraph2.style.display = 'block';
-    paragraph3.style.display = 'none';
-    button2.style.backgroundColor = '#EF7F1A';
-    button1.style.backgroundColor = 'transparent';
-    button3.style.backgroundColor = 'transparent';
+contribute_button.addEventListener('click', () => {
+    info_slides.style.display = 'none';
+    contribute_slides.style.display = 'block';
+    merch_slides.style.display = 'none';
+    contribute_button.style.backgroundColor = '#EF7F1A';
+    info_button.style.backgroundColor = 'transparent';
+    merch_button.style.backgroundColor = 'transparent';
 });
 
 next1.addEventListener('click', () => {
@@ -84,20 +84,20 @@ c_page4.style.display = 'none'
 
 });
 
-button3.addEventListener('click', () => {
-    paragraph1.style.display = 'none';
-    paragraph2.style.display = 'none';
-    paragraph3.style.display = 'block';
-    button3.style.backgroundColor = '#EF7F1A';
-    button1.style.backgroundColor = 'transparent';
-    button2.style.backgroundColor = 'transparent';
+merch_button.addEventListener('click', () => {
+    info_slides.style.display = 'none';
+    contribute_slides.style.display = 'none';
+    merch_slides.style.display = 'block';
+    merch_button.style.backgroundColor = '#EF7F1A';
+    info_button.style.backgroundColor = 'transparent';
+    contribute_button.style.backgroundColor = 'transparent';
 
 });
 
 button5.addEventListener('click', () => {
-    paragraph1.style.display = 'none';
-    paragraph2.style.display = 'none';
-    paragraph3.style.display = 'block';
+    info_slides.style.display = 'none';
+    contribute_slides.style.display = 'none';
+    merch_slides.style.display = 'block';
     button6.style.backgroundColor = '#EF7F1A';
     button5.style.backgroundColor = 'transparent';
 });
